afe86a74d14c137ec5d1743cc77a783b8861b441
Imad-prg <155386613+Imad-prg@users.noreply.github.com>
2025-04-14 20:53:09 +0200

    VALIDER

---
+++ ChenYI/animal.h
@@ -0,0 +1,8 @@
+typedef struct {
+    int num;
+    char nom[100];
+    Espece espece;                 
+    int annee_naissance;
+    int poids;          
+    char commentaire[100];
+}Animal;
---
+++ ChenYI/espece.h
@@ -0,0 +1,7 @@
+//structure des especes presentes;
+typedef enum {
+    CHIEN,
+    CHAT,
+    HAMSTER,
+    AUTRUCHE,
+} Espece; 
