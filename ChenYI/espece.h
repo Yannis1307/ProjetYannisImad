9c46cb2bc90922e2f130f568f1c75ce13b4b3bec
Yannis1307 <mistergameryl@gmail.com>
2025-04-20 16:49:31 +0200

    Update espece.h

--- ChenYI/espece.h
+++ ChenYI/espece.h
@@ -1,7 +1,7 @@
 //structure des especes presentes;
-#define NB_ESPECES 4
+#define NB_ESPECES 5
 #define MAX_ANIMAUX 50
 
 
-const char* ESPECES[NB_ESPECES] = {"Chien", "Chat", "Hamster", "Autruche"};
+const char* ESPECES[NB_ESPECES] = {"Chien", "Chat", "Hamster", "Autruche","Tortue"};
 const char* FICHIERS[NB_ESPECES] = {"ChenYI/chiens.txt", "ChenYI/chats.txt", "ChenYI/hamsters.txt", "ChenYI/autruches.txt"};
