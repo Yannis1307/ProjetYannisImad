abde3b100bf2ac77198873f2253a6758adec1a4d
Imad-prg <155386613+Imad-prg@users.noreply.github.com>
2025-05-10 11:45:45 +0200

    valider

--- ChenYI/espece.c
+++ ChenYI/espece.c
@@ -16,11 +16,18 @@ const char *get_espece(int i) {
 }
 
 const char *get_fichier(int i) {
-  const char *fichiers[] = { "chien.txt", "chat.txt", "hamster.txt", "autruche.txt", "tortue.txt" };
+  const char *fichiers[] = {
+    "animaux/chien.txt",
+    "animaux/chat.txt",
+    "animaux/hamster.txt",
+    "animaux/autruche.txt",
+    "animaux/tortue.txt"
+  };
+
   if (i >= 0 && i < nb_especes()) {
     return fichiers[i];
   } else {
     printf("Erreur : indice %d invalide pour les fichiers (max = %d).\n", i, nb_especes() - 1);
-    return "fichier_inconnu.txt";
+    return "animaux/fichier_inconnu.txt";
   }
 }
